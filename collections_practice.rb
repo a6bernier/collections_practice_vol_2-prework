8920832c7b2e87754f40a8e421f61861b0fb8600
Andrew Bernier <a6bernier@gmail.com>
2018-09-12 22:59:02 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,55 +1,62 @@
+
 def sort_array_asc(array)
   array.sort
 end
 
 def sort_array_desc(array)
-  array.sort.reverse
+  array.sort do |a, b|
+    b <=> a
+  end
 end
 
 def sort_array_char_count(array)
   array.sort do |a, b|
-    if a.length == b.length
-      0
-    elsif a.length < b.length
-      -1
-    elsif a.length > b.length
-      1
-    end
+    a.size <=> b.size
   end
 end
 
 def swap_elements(array)
-  second_e = array[1]
-  third_e = array[2]
+  new_second_elem = array[2]
+  new_third_item = array[1]
 
-  array[1] = third_e
-  array[2] = second_e
-  array
+  array[1] = new_second_elem
+  array[2] = new_third_item
+  return array
 end
 
 def reverse_array(array)
   array.reverse
 end
 
-def kesha_maker(array)
-  array.each { |e| e[2] = "$" }
-  array
+def kesha_maker(arr)
+  arr.collect do |i|
+    new_word = i.split("")
+    new_word[2] = "$"
+    new_word.join
+  end
 end
 
-def find_a(array)
-  array.select { |e| e.start_with?("a") }
+def find_a(arr)
+  new_array = []
+  arr.each_with_index do |word|
+    if word.split("")[0] == "a"
+      new_array << word
+    end
+  end
+  new_array
 end
 
-def sum_array(array)
-  array.inject { |sum, n| sum + n }
+def sum_array(arr)
+  arr.inject(0) do |r, e|
+    r + e
+  end
 end
-
-def add_s(array)
-  array.each_with_index.map do |e, i|
-    unless i == 1
-      e + "s"
-    else
+def add_s(arr)
+  arr.each_with_index.collect do |e, i|
+    if i == 1
       e
+    else
+      e+"s"
     end
   end
 end
